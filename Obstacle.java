5938f4d2bea9663ce31fe608935f3ad29dfc69cd
Andre Londoño <andrelondono98@gmail.com>
2016-06-07 11:31:34 -0700

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,75 +1,81 @@
+package com.example.andre.gyrogolf;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by andre on 6/3/16.
+ */
 public class Obstacle {
 
-	private float xCoord;
-	private float yCoord;
-	private float width;
-	private float height;
-	private float leftVert;
-	private float rightVert;
-	private float top;
-	private float bottom;
+    //make sure obstacles dont spawn over hole
+    //detect ball collision with obstacles
+    //make sure hole doesnt spawn outside of bounds
 
-	public Obstacle(float xCoord, float yCoord, float width, float height) {
-		this.xCoord = xCoord;
-		this.yCoord = yCoord;
-		this.width = width;
-		this.height = height;
-		leftVert = yCoord - (width / 2);
-		rightVert = leftVert + width;
-		top = xCoord - (height / 2);
-		bottom = top + height;
-	}
+    private float xCoord;
+    private float yCoord;
+    private float width;
+    private float height;
+    private float leftVert;
+    private float rightVert;
+    private float top;
+    private float bottom;
 
-	public int contact(float ballX, float ballY, float rad) {
-		if(xBounce(ballX, ballY, width, height))
-			return 1;
-		else if(yBounce(ballX, ballY, width, height))
-			return 2;
-		else
-			return 0;
-	}
-	public boolean xBounce(float ballX, float ballY, float rad) {
+    public Obstacle(float xCoord, float yCoord, float width, float height) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.width = width;
+        this.height = height;
+        leftVert = xCoord - (width / 2);
+        rightVert = leftVert + width;
+        top = yCoord - (height / 2);
+        bottom = top + height;
+    }
+
+    public int contact(float ballX, float ballY, float rad) {
+        if (xBounce(ballX, ballY, rad))
+            return 1;
+        else if(yBounce(ballX, ballY, rad))
+            return 2;
+        else
+            return 0;
+
+    }
+
+
+    public boolean xBounce(float ballX, float ballY, float rad) {
         if (ballX + rad > leftVert && ballX + rad < rightVert){
-            if (ballY > top && ballY < bottom)
+            if (ballY > top && ballY < bottom) {
+                System.out.println("&&&&&&&&&&& SOMETHING IS HAPPENING &&&&&&&&&&");
                 return true;
+            }
         }
         else if (ballX - rad > leftVert && ballX - rad < rightVert){
-            if (ballY > top && ballY < bottom)
+            if (ballY > top && ballY < bottom) {
+                System.out.println("&&&&&&&&&&& SOMETHING IS HAPPENING &&&&&&&&&&");
                 return true;
+            }
         }
             return false;
     }
     public boolean yBounce(float ballX, float ballY, float rad) {
         if (ballY + rad > top && ballY + rad < bottom){
-            if (ballX > leftVert && ballX < rightVert)
+            if (ballX > leftVert && ballX < rightVert) {
+                System.out.println("&&&&&&&&&&& SOMETHING IS HAPPENING &&&&&&&&&&");
                 return true;
+            }
         }
         else if (ballY - rad > top && ballY - rad < bottom) {
-            if (ballX > leftVert && ballX < rightVert)
+            if (ballX > leftVert && ballX < rightVert) {
+                System.out.println("&&&&&&&&&&& SOMETHING IS HAPPENING &&&&&&&&&&");
                 return true;
+            }
         }
 
             return false;
     }
 
-	public float getXCoord() {
-		return xCoord;
-	}
-	public float getYCoord() {
-		return yCoord;
-	}
 
-	/* public void change(float xCoord, float yCoord, float width, float height) {
-		this.xCoord = xCoord;
-		this.yCoord = yCoord;
-		this.width = width;
-		this.height = height;
-		leftVert = xCoord - (width / 2);
-		rightVert = leftVert + width;
-		top = yCoord - (height / 2);
-		bottom = top + height;
-	} */
-	public void draw(Canvas canvas, Paint paint) {
-		canvas.drawRect(leftVert, top, rightVert, bottom, paint);
-	}
-}
+
+
+
